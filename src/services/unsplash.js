8e65184dd7d8b51615e3e317979fee25b5c9e4bb
Bataino <bataino.ronaldo@gmail.com>
2024-10-12 09:38:18 +0100

    Await Removed

--- src/services/unsplash.js
+++ src/services/unsplash.js
@@ -1,5 +1,5 @@
 {
-var accessKey = await import.meta.env.VITE_UNSPLASH_KEY;
+var accessKey = import.meta.env.VITE_UNSPLASH_KEY;
 console.log("ACCESS", accessKey) // Replace with your Unsplash API Access Key
 var randomUrl = `https://api.unsplash.com/photos?client_id=${accessKey}&count=10`; // Fetch 10 random photos
 var searchUrl = `https://api.unsplash.com/search/photos?client_id=${accessKey}&count=10&query=`; //
